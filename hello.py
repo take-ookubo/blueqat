61dce9c580f5ba7ed8b76d519e8a0993e627e768
take-ookubo <take.ookubo@gmail.com>
2019-04-03 03:16:41 +0900

    Add HelloGate

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-from blueqat import Circuit
-print(Circuit().h[0].m[:].run(shots=100))
+from blueqat import BlueqatGlobalSetting
+
+class HelloGate(Gate):
+    lowername = "hello"
+    def fallback(self, n_qubits):
+        print("Hello")
+        return []
+
+BlueqatGlobalSetting.register_gate("hello", HelloGate)
+BlueqatGlobalSetting.register_gate("yeah", HelloGate)
+c = Circuit().hello[0].yeah[0]
+c.run()
